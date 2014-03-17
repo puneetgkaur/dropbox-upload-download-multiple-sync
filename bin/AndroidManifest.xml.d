c3420f223c4599e30624980af6a63ae37861bfb4
Puneet Kaur <puneet.gkaur@gmail.com>
2014-03-18 00:17:32 +0530

    removed bin

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/dr/development/phonegap-dropbox-sync-master_Paint App/alarm clock_to_sync1/bin/AndroidManifest.xml : \
-/home/dr/development/phonegap-dropbox-sync-master_Paint App/alarm clock_to_sync1/AndroidManifest.xml \
